e060c88b76c94d3aa23c9438de722a3b726f20f2
ReJacket <eljordin800@gmail.com>
2024-10-25 14:40:53 -0300

    Começo do PHP e também mudanças para algumas paginas para elas poderem estar com a label "Login/Cadastro" em todas as telas

---
+++ PHP/conexão.php
@@ -0,0 +1,20 @@
+<?php
+class Database {
+    private $host = 'localhost:3008';
+    private $dbname = '';
+    private $username = 'root';
+    private $password = '';
+    private $conn;
+
+    public function connect() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->dbname, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo 'Erro na conexão: ' . $e->getMessage();
+        }
+        return $this->conn;
+    }
+}
+?>
